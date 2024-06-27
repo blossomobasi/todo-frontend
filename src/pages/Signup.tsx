6d43c88eea840a96dde403f00f9512419c5a0fbb
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-27 11:34:45 +0100

    useSignup hook to register a user in the Signup component

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,20 +1,51 @@
+import { useState } from "react";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
+import { useSignup } from "../hooks/auth/useSignup";
 
 function Signup() {
+    const { signup, isSigningUp } = useSignup();
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     function handleSignup(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
+
+        signup({ username, email, password });
     }
 
     return (
         <div className="w-[22rem] mt-28">
             <h1 className="text-center my-5 text-2xl">Signup</h1>
             <form className="flex flex-col space-y-5" onSubmit={handleSignup}>
-                <Input type="text" placeholder="Username" />
-                <Input type="email" placeholder="Email" />
-                <Input type="password" placeholder="Password" />
+                <Input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    disabled={isSigningUp}
+                />
+                <Input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    disabled={isSigningUp}
+                />
+                <Input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    disabled={isSigningUp}
+                />
 
-                <Button type="submit" className="bg-[#9e78cf] hover:bg-[#3e1671]">
+                <Button
+                    type="submit"
+                    disabled={isSigningUp}
+                    className="bg-[#9e78cf] hover:bg-[#3e1671]"
+                >
                     Signup
                 </Button>
             </form>
