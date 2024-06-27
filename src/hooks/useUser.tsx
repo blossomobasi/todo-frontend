6c43e445db6b6e7c72035a620833a9b4a7792c45
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-27 11:19:04 +0100

    Add useUser hook

---
+++ src/hooks/useUser.tsx
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { getMe } from "../api/auth";
+import { UserResponse } from "../types/user";
+
+export function useUser() {
+    const { data, isLoading, error } = useQuery<UserResponse, Error>({
+        queryKey: ["user"],
+        queryFn: getMe,
+    });
+
+    return {
+        user: data?.data.user,
+        isAuthenticated: data?.data.user.role.includes("user"),
+        isLoading,
+        error,
+    };
+}
