b2b8993ee1b39611e7b92e44803896af37df113e
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-27 11:10:26 +0100

    Add react query

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,38 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { UserProvider } from "./contexts/useUser";
 import ProtectedRoutes from "./components/ProtectedRoutes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-    return (
-        <UserProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <ProtectedRoutes>
-                                <AppLayout />
-                            </ProtectedRoutes>
-                        }
-                    />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                retry: 1,
+            },
+        },
+    });
 
-                    <Route path="*" element={<h1>Not Found</h1>} />
-                </Routes>
-            </BrowserRouter>
-        </UserProvider>
+    return (
+        <QueryClientProvider client={queryClient}>
+            <UserProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <ProtectedRoutes>
+                                    <AppLayout />
+                                </ProtectedRoutes>
+                            }
+                        />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+
+                        <Route path="*" element={<h1>Not Found</h1>} />
+                    </Routes>
+                </BrowserRouter>
+            </UserProvider>
+        </QueryClientProvider>
     );
 }
 
