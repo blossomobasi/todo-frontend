28eb2db09ab950916ece880812edc35d9f05e1d2
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-27 10:48:38 +0100

    Protect App

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,21 @@ import AppLayout from "./components/AppLayout";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { UserProvider } from "./contexts/useUser";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
     return (
         <UserProvider>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<AppLayout />} />
+                    <Route
+                        path="/"
+                        element={
+                            <ProtectedRoutes>
+                                <AppLayout />
+                            </ProtectedRoutes>
+                        }
+                    />
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<Signup />} />
 
