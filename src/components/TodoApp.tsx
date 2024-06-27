3c7000efada42a31a9f6e7ea152b3274d5ba7aeb
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-27 10:49:20 +0100

    Update: Display username in the page

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,10 +1,13 @@
+import { useUser } from "../contexts/useUser";
 import AddTodo from "./AddTodo";
 import Todos from "./Todos";
 
 function TodoApp() {
+    const { user } = useUser();
+
     return (
         <div>
-            <h1 className="text-2xl text-center my-5">Welcome %User%</h1>
+            <h1 className="text-2xl mt-14">Welcome, {user?.username}</h1>
 
             <AddTodo />
             <Todos />
