4ba43444922e06161d6cebb25b285cf3f0ad012a
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-28 05:57:19 +0100

    Add api to update a todo and to delete a todo

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -26,3 +26,33 @@ export async function createTodo(todo: TodoInput) {
 
     return res.json();
 }
+
+export async function updateTodo(id: string, todo: TodoInput) {
+    const res = await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/v1/todos/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include", // This is required for cookies to be sent
+        body: JSON.stringify(todo),
+    });
+
+    if (!res.ok) {
+        throw new Error("Failed to update todo");
+    }
+
+    return res.json();
+}
+
+export async function deleteTodo(id: string) {
+    const res = await fetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/v1/todos/${id}`, {
+        method: "DELETE",
+        credentials: "include", // This is required for cookies to be sent
+    });
+
+    if (!res.ok) {
+        throw new Error("Failed to delete todo");
+    }
+
+    return res.json();
+}
