d14aa6ee05534df4af36febdd1b251053efc5ea9
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-28 13:34:54 +0100

    Update: Configure toster and add DateProvider

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Signup from "./pages/Signup";
 import ProtectedRoutes from "./components/ProtectedRoutes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { DateProvider } from "./context/DateContext";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
     const queryClient = new QueryClient({
@@ -18,22 +20,26 @@ function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <ReactQueryDevtools initialIsOpen={false} />
-            <BrowserRouter>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <ProtectedRoutes>
-                                <AppLayout />
-                            </ProtectedRoutes>
-                        }
-                    />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
+            <Toaster />
 
-                    <Route path="*" element={<h1>Not Found</h1>} />
-                </Routes>
-            </BrowserRouter>
+            <DateProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <ProtectedRoutes>
+                                    <AppLayout />
+                                </ProtectedRoutes>
+                            }
+                        />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+
+                        <Route path="*" element={<h1>Not Found</h1>} />
+                    </Routes>
+                </BrowserRouter>
+            </DateProvider>
         </QueryClientProvider>
     );
 }
