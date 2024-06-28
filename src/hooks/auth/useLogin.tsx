bf103501b801f0d37a1c19c8260f1db4222ea4f5
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-28 13:37:14 +0100

    Update: Use toaster when user successfully logs in

--- src/hooks/auth/useLogin.tsx
+++ src/hooks/auth/useLogin.tsx
@@ -2,9 +2,11 @@ import { useMutation } from "@tanstack/react-query";
 import { login as loginApi } from "../../api/auth";
 import { AuthResponse, Login } from "../../types/auth";
 import { useNavigate } from "react-router-dom";
+import { useToast } from "../../components/ui/use-toast";
 
 export function useLogin() {
     const navigate = useNavigate();
+    const { toast } = useToast();
     const {
         mutate: login,
         isPending: isLoggingIn,
@@ -12,7 +14,12 @@ export function useLogin() {
     } = useMutation<AuthResponse, Error, Login>({
         mutationFn: loginApi,
         onSuccess: () => {
-            console.log("Logged in successfully");
+            // console.log("Logged in successfully");
+            toast({
+                title: "Authentication",
+                description: "Successfully Logged In",
+            });
+
             navigate("/");
         },
         onError: (error) => {
