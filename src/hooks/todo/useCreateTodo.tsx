e89e4be499a5e6e75abf082e0d20e5d73f44e35f
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-28 13:37:54 +0100

    Add types to mutation in useCreateTodo hook

--- src/hooks/todo/useCreateTodo.tsx
+++ src/hooks/todo/useCreateTodo.tsx
@@ -1,9 +1,14 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createTodo as createTodoApi } from "../../api/todos";
+import { TodoInput, TodoResponse } from "../../types/todos";
 
 export function useCreateTodo() {
     const queryClient = useQueryClient();
-    const { mutate: createTodo, isPending: isCreatingTodo } = useMutation({
+    const { mutate: createTodo, isPending: isCreatingTodo } = useMutation<
+        TodoResponse,
+        Error,
+        TodoInput
+    >({
         mutationFn: createTodoApi,
         onSuccess: () => {
             queryClient.invalidateQueries({
