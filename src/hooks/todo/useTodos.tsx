979815b320b4430fd2333ed84188deda1a887fec
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-29 12:57:31 +0100

    Update code to fetch todo if there is a user

--- src/hooks/todo/useTodos.tsx
+++ src/hooks/todo/useTodos.tsx
@@ -1,11 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { getUserTodos } from "../../api/todos";
 import { TodosResponse } from "../../types/todos";
+import { useUser } from "../auth/useUser";
 
 export function useTodos() {
+    const { user } = useUser();
     const { data, isLoading, error } = useQuery<TodosResponse, Error>({
-        queryKey: ["todos"],
+        queryKey: ["todos", user?._id],
         queryFn: getUserTodos,
+        enabled: !!user?._id,
     });
 
     return {
