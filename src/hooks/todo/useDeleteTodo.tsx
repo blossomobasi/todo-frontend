8f7df6a8b4123c92563093a21aa571d88f02d957
Obasi Chizitere <blossomobasi2@gmail.com>
2024-06-30 16:54:12 +0100

    Add toast onError and onSuccess

--- src/hooks/todo/useDeleteTodo.tsx
+++ src/hooks/todo/useDeleteTodo.tsx
@@ -1,7 +1,9 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteTodo } from "../../api/todos";
+import { useToast } from "../../components/ui/use-toast";
 
 export function useDeleteTodo() {
+    const { toast } = useToast();
     const queryClient = useQueryClient();
     const { mutate, isPending } = useMutation({
         mutationFn: deleteTodo,
@@ -9,9 +11,17 @@ export function useDeleteTodo() {
             queryClient.invalidateQueries({
                 queryKey: ["todos"],
             });
+
+            toast({
+                title: "Task",
+                description: "Task was successfully deleted",
+            });
         },
         onError: (err) => {
-            console.error(err);
+            toast({
+                title: "Task",
+                description: err.message || "Failed to delete task",
+            });
         },
     });
 
