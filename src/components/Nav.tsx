b0d8e93e6f55b4396eddd93ea019e42c78e0f6ff
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-01 00:52:38 +0100

    Update: Display login and signup button if user is logged out.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,44 @@
 import { LogOut } from "lucide-react";
 import { useLogout } from "../hooks/auth/useLogout";
+import { useUser } from "../hooks/auth/useUser";
+import { Button } from "./ui/button";
+import { useNavigate } from "react-router-dom";
 
 function Nav() {
     const { logout, isLoggingOut } = useLogout();
+    const { user } = useUser();
+    const navigate = useNavigate();
 
     return (
         <nav>
             <ul className="flex items-center justify-between my-5">
                 <li>blossom.dev</li>
-                <span title="logout">
-                    <LogOut
-                        className={`hover:text-[#3e1671] cursor-pointer ${
-                            isLoggingOut && "cursor-not-allowed opacity-50"
-                        }`}
-                        onClick={() => logout()}
-                    />
-                </span>
+
+                {user ? (
+                    <span title="logout">
+                        <LogOut
+                            className={`hover:text-[#4d3967] cursor-pointer ${
+                                isLoggingOut && "cursor-not-allowed opacity-50"
+                            }`}
+                            onClick={() => logout()}
+                        />
+                    </span>
+                ) : (
+                    <div className="flex space-x-3">
+                        <Button
+                            className="bg-transparent hover:bg-[#3e1671]"
+                            onClick={() => navigate("/login")}
+                        >
+                            Login
+                        </Button>
+                        <Button
+                            className="bg-[#9e78cf] hover:bg-[#3e1671]"
+                            onClick={() => navigate("/signup")}
+                        >
+                            Signup
+                        </Button>
+                    </div>
+                )}
             </ul>
         </nav>
     );
