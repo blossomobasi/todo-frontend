acb809705cf78edc860f44394bfc311efd16d66f
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-01 00:38:51 +0100

    Update: Use Nav component in the layout

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,25 +1,10 @@
-import { LogOut } from "lucide-react";
+import Nav from "./Nav";
 import TodoApp from "./TodoApp";
-import { useLogout } from "../hooks/auth/useLogout";
 
 function AppLayout() {
-    const { logout, isLoggingOut } = useLogout();
-
     return (
         <main className="p-1 sm:w-[26rem] w-full">
-            <nav>
-                <ul className="flex items-center justify-between my-5">
-                    <li>blossom.dev</li>
-                    <span title="logout">
-                        <LogOut
-                            className={`hover:text-[#3e1671] cursor-pointer ${
-                                isLoggingOut && "cursor-not-allowed opacity-50"
-                            }`}
-                            onClick={() => logout()}
-                        />
-                    </span>
-                </ul>
-            </nav>
+            <Nav />
             <TodoApp />
         </main>
     );
