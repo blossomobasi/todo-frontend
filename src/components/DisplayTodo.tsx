51a13d7c9190a760f4919a0759929f306c3483bf
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-02 11:52:16 +0100

    Refactor: refactor updateTodo

--- src/components/DisplayTodo.tsx
+++ src/components/DisplayTodo.tsx
@@ -1,19 +1,31 @@
-import { Bell, Check, Trash } from "lucide-react";
+import { Bell, Check, LoaderCircle, Trash } from "lucide-react";
 import { dateFormatter } from "../utils";
 import { Todo } from "../types/todos";
 import { useState } from "react";
 import { UpdateTodo } from "./UpdateTodo";
+import { useUpdateTodo } from "../hooks/todo/useUpdateTodo";
 
 type DisplayTodoProps = {
     todos: Todo[] | undefined;
     onDelete: (id: string) => void;
-    onComplete: (id: string, status: boolean) => void;
-    onUpdate: (id: string, description: string, reminder: string | undefined) => void;
     isLoading: boolean;
 };
 
-function DisplayTodo({ todos, onDelete, onComplete, onUpdate, isLoading }: DisplayTodoProps) {
+function DisplayTodo({ todos, onDelete, isLoading }: DisplayTodoProps) {
     const [openUpdateTodo, setOpenUpdateTodo] = useState<string | null>(null);
+    const { isUpdatingTodo, updateTodo } = useUpdateTodo();
+    const [currentTodoId, setCurrentTodoId] = useState<string | null>(null);
+
+    function handleUpdateTodo(todoId: string, todo: Todo) {
+        setCurrentTodoId(todoId);
+
+        updateTodo({
+            todoId,
+            updateTodoInput: {
+                completed: !todo.completed,
+            },
+        });
+    }
 
     return (
         <div>
@@ -23,7 +35,6 @@ function DisplayTodo({ todos, onDelete, onComplete, onUpdate, isLoading }: Displ
                         <UpdateTodo
                             currentTodoId={todo._id}
                             todo={todo}
-                            onUpdate={onUpdate}
                             setOpenUpdateTodo={setOpenUpdateTodo}
                         />
                     ) : (
@@ -39,10 +50,14 @@ function DisplayTodo({ todos, onDelete, onComplete, onUpdate, isLoading }: Displ
                                     {todo.description}
                                 </p>
                                 <span className="flex items-center space-x-2">
-                                    <Check
-                                        className="hover:text-[#3e1671] cursor-pointer"
-                                        onClick={() => onComplete(todo._id, todo.completed)}
-                                    />
+                                    {currentTodoId === todo._id && isUpdatingTodo ? (
+                                        <LoaderCircle className="animate-spin" />
+                                    ) : (
+                                        <Check
+                                            className="hover:text-[#3e1671] cursor-pointer"
+                                            onClick={() => handleUpdateTodo(todo._id, todo)}
+                                        />
+                                    )}
                                     <Trash
                                         className={`hover:text-[#3e1671] cursor-pointer ${
                                             isLoading ? "opacity-50 cursor-not-allowed" : ""
