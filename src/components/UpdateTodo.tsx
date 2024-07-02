36153f7029fde61590d166eead906f4dd00c4dd9
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-02 11:53:07 +0100

    Refactor update todo

--- src/components/UpdateTodo.tsx
+++ src/components/UpdateTodo.tsx
@@ -3,32 +3,47 @@ import { Todo } from "../types/todos";
 import { Button } from "./ui/button";
 import { Switch } from "./ui/switch";
 import Calender from "./Calender";
+import { useUpdateTodo } from "../hooks/todo/useUpdateTodo";
+import { LoaderCircle } from "lucide-react";
 
 type UpdateTodoProps = {
     currentTodoId: string;
     todo: Todo;
-    onUpdate: (id: string, description: string, reminder: string | undefined) => void;
     setOpenUpdateTodo: (value: string | null) => void;
 };
 
-export function UpdateTodo({ currentTodoId, todo, onUpdate, setOpenUpdateTodo }: UpdateTodoProps) {
+export function UpdateTodo({ currentTodoId, todo, setOpenUpdateTodo }: UpdateTodoProps) {
     const [description, setDescription] = useState<string>(todo.description);
     const [reminder, setReminder] = useState<boolean>(todo.reminder ? true : false);
 
     const [startDate, setStartDate] = useState<Date>((todo.reminder || "") as unknown as Date);
 
+    const { isUpdatingTodo, updateTodo } = useUpdateTodo();
+
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
 
-        onUpdate(currentTodoId, description, todo.reminder);
+        // onUpdate(currentTodoId, description, todo.reminder);
+
+        updateTodo({
+            todoId: currentTodoId,
+            updateTodoInput: {
+                description,
+                reminder: reminder ? todo.reminder : undefined,
+            },
+        });
+
+        setOpenUpdateTodo(null);
     }
 
     return (
         <form onSubmit={handleSubmit} className="mb-2">
             <div className="flex flex-col space-y-3">
                 <textarea
-                    className="w-full bg-transparent border border-[#9e78cf] rounded-lg p-2"
-                    value={description}
+                    className="w-full bg-transparent border border-[#9e78cf] rounded-lg p-2 focus-within:outline-none"
+                    autoFocus
+                    // value={description}
+                    defaultValue={description}
                     onChange={(e) => setDescription(e.target.value)}
                 ></textarea>
 
@@ -39,24 +54,32 @@ export function UpdateTodo({ currentTodoId, todo, onUpdate, setOpenUpdateTodo }:
                     >
                         Cancel
                     </Button>
-                    <Button type="submit" className="bg-[#9e78cf] hover:bg-[#3e1671]">
-                        Update
+                    <Button
+                        type="submit"
+                        className={`bg-[#9e78cf] hover:bg-[#3e1671] ${
+                            isUpdatingTodo && "opacity-50"
+                        }`}
+                        disabled={isUpdatingTodo}
+                    >
+                        {isUpdatingTodo ? (
+                            <LoaderCircle className="animate-spin" size={20} />
+                        ) : (
+                            "Update"
+                        )}
                     </Button>
                 </div>
             </div>
 
             <div className="flex flex-col items-end space-y-3">
                 <div className="mt-5 flex items-center space-x-5 justify-end">
-                    <label htmlFor="reminder">reminder</label>
+                    <label htmlFor="update-reminder">reminder</label>
                     <Switch
-                        id="reminder"
+                        id="update-reminder"
                         checked={reminder}
                         onClick={() => setReminder(!reminder)}
                     />
                 </div>
-                <div>
-                    {reminder && <Calender startDate={startDate} setStartDate={setStartDate} />}
-                </div>
+                {reminder && <Calender startDate={startDate} setStartDate={setStartDate} />}
             </div>
         </form>
     );
